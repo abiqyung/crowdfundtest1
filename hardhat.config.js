f1339a9cfc3799682d51ac55d39fcf4b014db68d
abiqyung <abiqyung@gmail.com>
2024-11-14 23:46:08 +0700

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,36 +1,9 @@
 require("@matterlabs/hardhat-zksync-solc");
+require("dotenv").config(); // Load environment variables
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
+  defaultNetwork: "sepolia", // Moved here
   solidity: {
     version: "0.8.17",
     settings: {
@@ -38,6 +11,14 @@ module.exports = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  networks: {
+    hardhat: {}, // Local Hardhat network
+    sepolia: {
+      // Sepolia testnet configuration
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Environment variable for private key
     },
   },
 };
